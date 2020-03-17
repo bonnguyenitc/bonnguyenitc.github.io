9092976b691a9547220ddcce2fd1dfa3db94bacf
bonnguyenitc <bonnguyenitc@gmail.com>
2020-03-18 02:18:04 +0700

    first commit

---
+++ precache-manifest.303aacad3879781a8c511bb9c121081a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "426a4d6f02c828631e3d2d7d63a19e16",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4bbb05ab9dd1f1d53325",
+    "url": "/static/css/main.0b9796b5.chunk.css"
+  },
+  {
+    "revision": "b92f3b6d59e225e48a74",
+    "url": "/static/js/2.a25b69f3.chunk.js"
+  },
+  {
+    "revision": "a5288a4ed4e4beba8413aa5c9ae969ea",
+    "url": "/static/js/2.a25b69f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4bbb05ab9dd1f1d53325",
+    "url": "/static/js/main.23a0e460.chunk.js"
+  },
+  {
+    "revision": "71455ac8f714bf4c2b64",
+    "url": "/static/js/runtime-main.0ee9cc4a.js"
+  }
+]);
